e1c609bcac7b9c122bfc8019bea283a476d45f3e
JKS-dev <dms2535saini@gmail.com>
2024-06-17 18:48:39 +0530

    ,

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/react-firebase-chat/',
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+  },
+  publicDir: 'public',
+  base: '/react-firebase-chat/', // Replace with your repository name
+});
